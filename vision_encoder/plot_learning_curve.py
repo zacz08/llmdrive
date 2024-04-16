b5a8117f120d482afa3219dc4631bd9cdb57796c
C Zhang <rrg517@login2.viking2.yor.alces.network>
2024-04-16 17:36:31 +0100

    plot learning curve based on training log generated by Viking

--- vision_encoder/plot_learning_curve.py
+++ vision_encoder/plot_learning_curve.py
@@ -13,20 +13,21 @@ file_path = 'LMDrive/log.txt'
 def get_loss_array(loss='traffic'): 
     
     if loss in ['traffic', 'light', 'waypoint', 'total']:
-        loss_cache = []
+        loss_val, loss_avg = [], []
         with open(file_path, 'r') as file:
             for line in file:
                 if loss == 'total':
-                    match = re.search(r'Loss\:\s+(\d+\.\d+)', line)
+                    match = re.search(r'Loss:\s+(\d+\.\d+)\s+\((\d+\.\d+)\)', line)
                 elif loss == 'traffic':
-                    match = re.search(r'Loss\(traffic\):\s+(\d+\.\d+)', line)
+                    match = re.search(r'Loss\(traffic\):\s+(\d+\.\d+)\s+\((\d+\.\d+)\)', line)
                 elif loss == 'light':
-                    match = re.search(r'Loss\(light\):\s+(\d+\.\d+)', line)
+                    match = re.search(r'Loss\(light\):\s+(\d+\.\d+)\s+\((\d+\.\d+)\)', line)
                 elif loss == 'waypoint':
-                    match = re.search(r'Loss\(waypoints\):\s+(\d+\.\d+)', line)
+                    match = re.search(r'Loss\(waypoints\):\s+(\d+\.\d+)\s+\((\d+\.\d+)\)', line)
                 if match:
-                    loss_cache.append(float(match.group(1)))
-        return loss_cache
+                    loss_val.append(float(match.group(1)))
+                    loss_avg.append(float(match.group(2)))
+        return loss_val, loss_avg
     
     else:
         print("Invalid input")
@@ -34,22 +35,34 @@ def get_loss_array(loss='traffic'):
         
         
 def main():
-    traffic_loss = get_loss_array(loss='traffic')
-    trafficlight_loss = get_loss_array(loss='light')
-    waypoint_loss = get_loss_array(loss='waypoint')
-    total_loss = get_loss_array(loss='total')
+    traffic_loss_val, traffic_loss_avg = get_loss_array(loss='traffic')
+    trafficlight_loss_val, trafficlight_loss_avg = get_loss_array(loss='light')
+    waypoint_loss_val, waypoint_loss_avg = get_loss_array(loss='waypoint')
+    total_loss_val, total_loss_avg = get_loss_array(loss='total')
     
     plt.figure(figsize=(10, 5))
-    plt.plot(traffic_loss, marker='o', color='b', label='Loss(traffic)')
-    plt.plot(trafficlight_loss, marker='o', color='r', label='Loss(light)')
-    plt.plot(waypoint_loss, marker='o', color='y', label='Loss(waypoint)')
-    plt.plot(total_loss, marker='o', color='black', label='total')
-    
-    plt.title('Learning Curve of Loss')
+    plt.subplot(1, 2, 1)
+    plt.plot(traffic_loss_val, marker='o', color='b', label='Loss(traffic)')
+    plt.plot(trafficlight_loss_val, marker='o', color='r', label='Loss(light)')
+    plt.plot(waypoint_loss_val, marker='o', color='y', label='Loss(waypoint)')
+    plt.plot(total_loss_val, marker='o', color='black', label='total')
+    plt.title('Learning Curve of Loss (val)')
     plt.xlabel('Training Batch')
     plt.ylabel('Loss')
     plt.grid(True)
     plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(traffic_loss_avg, marker='o', color='b', label='Loss(traffic)')
+    plt.plot(trafficlight_loss_avg, marker='o', color='r', label='Loss(light)')
+    plt.plot(waypoint_loss_avg, marker='o', color='y', label='Loss(waypoint)')
+    plt.plot(total_loss_avg, marker='o', color='black', label='total')
+    plt.title('Learning Curve of Loss (avg)')
+    plt.xlabel('Training Batch')
+    plt.ylabel('Loss')
+    plt.grid(True)
+    plt.legend()
+    
     plt.tight_layout()
     plt.show()
     
