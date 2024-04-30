7f0613d9014fb8b04fad99aa4e5f4b8100f5319d
zacharyzhang95 <zacharyzhang95@163.com>
2024-04-30 20:59:28 +0100

    update data generation tool for leaderboard 1.0

--- data_collection/bash_tools.py
+++ data_collection/bash_tools.py
@@ -1,34 +1,6 @@
 import os
 import random
 
-##### number of server #####
-server_num = 1
-
-
-routes = {}
-# routes["training_routes/routes_town01_short.xml"] = "scenarios/town01_all_scenarios.json"
-routes["training_routes/routes_town01_tiny.xml"] = "scenarios/town01_all_scenarios.json"
-# routes["training_routes/routes_town02_short.xml"] = "scenarios/town02_all_scenarios.json"
-routes["training_routes/routes_town02_tiny.xml"] = "scenarios/town02_all_scenarios.json"
-# routes["training_routes/routes_town03_short.xml"] = "scenarios/town03_all_scenarios.json"
-# routes["training_routes/routes_town03_tiny.xml"] = "scenarios/town03_all_scenarios.json"
-# routes["training_routes/routes_town04_short.xml"] = "scenarios/town04_all_scenarios.json"
-# routes["training_routes/routes_town04_tiny.xml"] = "scenarios/town04_all_scenarios.json"
-# routes["training_routes/routes_town05_short.xml"] = "scenarios/town05_all_scenarios.json"
-# routes["training_routes/routes_town05_tiny.xml"] = "scenarios/town05_all_scenarios.json"
-# routes["training_routes/routes_town05_long.xml"] = "scenarios/town05_all_scenarios.json"
-# routes["training_routes/routes_town06_short.xml"] = "scenarios/town06_all_scenarios.json"
-# routes["training_routes/routes_town06_tiny.xml"] = "scenarios/town06_all_scenarios.json"
-# routes["training_routes/routes_town07_short.xml"] = "scenarios/town07_all_scenarios.json"
-# routes["training_routes/routes_town07_tiny.xml"] = "scenarios/town07_all_scenarios.json"
-# routes["training_routes/routes_town10_short.xml"] = "scenarios/town10_all_scenarios.json"
-# routes["training_routes/routes_town10_tiny.xml"] = "scenarios/town10_all_scenarios.json"
-# routes["additional_routes/routes_town01_long.xml"] = "scenarios/town01_all_scenarios.json"
-# routes["additional_routes/routes_town02_long.xml"] = "scenarios/town02_all_scenarios.json"
-# routes["additional_routes/routes_town03_long.xml"] = "scenarios/town03_all_scenarios.json"
-# routes["additional_routes/routes_town04_long.xml"] = "scenarios/town04_all_scenarios.json"
-# routes["additional_routes/routes_town06_long.xml"] = "scenarios/town06_all_scenarios.json"
-
 ip_ports = []
 
 for port in range(2000, 2008, 2):
