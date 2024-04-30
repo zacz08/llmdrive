a0774e3523cb138941fe1afbe02e1681f4606cd7
zacharyzhang95 <zacharyzhang95@163.com>
2024-04-30 20:06:25 +0100

    update data generation tool for leaderboard 1.0

--- dataset/init_dir.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for i in range(4):
-    if not os.path.exists("sub-%d" % i):
-        os.mkdir("sub-%d" % i)
-    if not os.path.exists("sub-%d/results" % i):
-        os.mkdir("sub-%d/results" % i)
